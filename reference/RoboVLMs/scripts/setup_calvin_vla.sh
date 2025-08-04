8ece1b0dc632f034cfcf72ec95cc9fc591534448
idejie <i@idejie.com>
2025-08-04 16:35:47 +0000

    fix install deps

--- reference/RoboVLMs/scripts/setup_calvin_vla.sh
+++ reference/RoboVLMs/scripts/setup_calvin_vla.sh
@@ -7,4 +7,21 @@ apt-get -yqq install libegl1 libgl1
 apt-get install -yqq libegl1-mesa libegl1-mesa-dev
 apt-get install -yqq mesa-utils libosmesa6-dev llvm
 apt-get -yqq install meson
+# You source un-comment the `dep-src` in /etc/apt/sources.list
 apt-get -yqq build-dep mesa
+
+conda install -c conda-forge gcc=12.1.0 gxx_linux-64 -y
+
+cd ${project_root}/reference/RoboVLMs/calvin
+
+sh install.sh
+
+# CALVIN spesicifcally requires the following version of numpy
+pip install numpy==1.21.0
+
+# Download dataset
+cd ${project_root}/reference/RoboVLMs/calvin/dataset
+# sh download_data.sh debug
+# sh download_data.sh D
+# sh download_data.sh ABC
+sh download_data.sh ABCD
